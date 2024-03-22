02a80d795e23342bd6892353a3419f00a5143c83
Kenny <kennypvh@gmail.com>
2024-03-22 11:34:47 -0300

    Add nav_msgs/Odometry include and update publisher topic

--- rpl_exploration/src/rpl_exploration.cpp
+++ rpl_exploration/src/rpl_exploration.cpp
@@ -14,6 +14,7 @@
 #include <rrtplanner/rrtAction.h>
 
 #include <geometry_msgs/PoseStamped.h>
+#include <nav_msgs/Odometry.h>
 
 #include <nav_msgs/Path.h>
 #include <tf2/utils.h>
@@ -30,13 +31,9 @@ int main(int argc, char** argv)
   logfile.open(path + "/data/logfile.csv");
   pathfile.open(path + "/data/path.csv");
 
-  ros::Publisher pub(nh.advertise<geometry_msgs::PoseStamped>("/mavros/"
-                                                              "setpoint_position/"
-                                                              "local",
-                                                              1000));
+  ros::Publisher pub(nh.advertise<mrs_msgs::ReferenceStamped>("/uav1/control_manager/reference", 1000));
 
-  ros::ServiceClient coverage_srv =
-      nh.serviceClient<aeplanner_evaluation::Coverage>("/get_coverage");
+  ros::ServiceClient coverage_srv = nh.serviceClient<aeplanner_evaluation::Coverage>("/get_coverage");
 
   // wait for fly_to server to start
   // ROS_INFO("Waiting for fly_to action server");
@@ -46,8 +43,7 @@ int main(int argc, char** argv)
 
   // wait for aep server to start
   ROS_INFO("Waiting for aeplanner action server");
-  actionlib::SimpleActionClient<aeplanner::aeplannerAction> aep_ac("make_plan",
-                                                                   true);
+  actionlib::SimpleActionClient<aeplanner::aeplannerAction> aep_ac("make_plan", true);
   aep_ac.waitForServer();  // will wait for infinite time
   ROS_INFO("aeplanner action server started!");
 
@@ -58,16 +54,14 @@ int main(int argc, char** argv)
   ROS_INFO("rrt Action server started!");
 
   // Get current pose
-  geometry_msgs::PoseStamped::ConstPtr init_pose =
-      ros::topic::waitForMessage<geometry_msgs::PoseStamped>("/mavros/"
-                                                             "local_position/pose");
-  double init_yaw = tf2::getYaw(init_pose->pose.orientation);
+  nav_msgs::Odometry::ConstPtr init_pose =
+      ros::topic::waitForMessage<nav_msgs::Odometry>("/uav1/control_manager/control_reference");
+  // double init_yaw = tf2::getYaw(init_pose->pose.orientation);
+  double init_yaw = tf::getYaw(init_pose->pose.pose.orientation);
   // Up 2 meters and then forward one meter
   double initial_positions[8][4] = {
-    { init_pose->pose.position.x, init_pose->pose.position.y,
-      init_pose->pose.position.z + 2.0, init_yaw },
-    { init_pose->pose.position.x + 1.0 * std::cos(init_yaw),
-      init_pose->pose.position.y + 1.0 * std::sin(init_yaw),
+    { init_pose->pose.position.x, init_pose->pose.position.y, init_pose->pose.position.z + 2.0, init_yaw },
+    { init_pose->pose.position.x + 1.0 * std::cos(init_yaw), init_pose->pose.position.y + 1.0 * std::sin(init_yaw),
       init_pose->pose.position.z + 2.0, init_yaw },
   };
 
@@ -82,8 +76,7 @@ int main(int argc, char** argv)
     goal.pose.pose.position.x = initial_positions[i][0];
     goal.pose.pose.position.y = initial_positions[i][1];
     goal.pose.pose.position.z = initial_positions[i][2];
-    goal.pose.pose.orientation =
-        tf::createQuaternionMsgFromYaw(initial_positions[i][3]);
+    goal.pose.pose.orientation = tf::createQuaternionMsgFromYaw(initial_positions[i][3]);
     last_pose.pose = goal.pose.pose;
 
     ROS_INFO_STREAM("Sending initial goal...");
@@ -121,8 +114,8 @@ int main(int argc, char** argv)
       ros::Time s = ros::Time::now();
       geometry_msgs::PoseStamped goal_pose = aep_ac.getResult()->pose;
       // Write path to file
-      pathfile << goal_pose.pose.position.x << ", " << goal_pose.pose.position.y
-               << ", " << goal_pose.pose.position.z << ", n" << std::endl;
+      pathfile << goal_pose.pose.position.x << ", " << goal_pose.pose.position.y << ", " << goal_pose.pose.position.z
+               << ", n" << std::endl;
 
       last_pose.pose = goal_pose.pose;
       rpl_exploration::FlyToGoal goal;
@@ -144,8 +137,7 @@ int main(int argc, char** argv)
         ROS_WARN("Exploration complete!");
         break;
       }
-      for (auto it = aep_ac.getResult()->frontiers.poses.begin();
-           it != aep_ac.getResult()->frontiers.poses.end(); ++it)
+      for (auto it = aep_ac.getResult()->frontiers.poses.begin(); it != aep_ac.getResult()->frontiers.poses.end(); ++it)
       {
         rrt_goal.goal_poses.poses.push_back(*it);
       }
@@ -162,8 +154,8 @@ int main(int argc, char** argv)
       {
         geometry_msgs::Pose goal_pose = path.poses[i].pose;
         // Write path to file
-        pathfile << goal_pose.position.x << ", " << goal_pose.position.y << ", "
-                 << goal_pose.position.z << ", f" << std::endl;
+        pathfile << goal_pose.position.x << ", " << goal_pose.position.y << ", " << goal_pose.position.z << ", f"
+                 << std::endl;
 
         last_pose.pose = goal_pose;
         rpl_exploration::FlyToGoal goal;
@@ -178,21 +170,17 @@ int main(int argc, char** argv)
 
     ros::Duration elapsed = ros::Time::now() - start;
 
-    ROS_INFO_STREAM("Iteration: "       << iteration << "  " <<
-                    "Time: "            << elapsed << "  " <<
-                    "Sampling: "        << aep_ac.getResult()->sampling_time.data << "  " <<
-                    "Planning: "        << aep_ac.getResult()->planning_time.data << "  " <<
-                    "Collision check: " << aep_ac.getResult()->collision_check_time.data  << "  " <<
-                    "Flying: "          << fly_time << " " <<
-                    "Tree size: "       << aep_ac.getResult()->tree_size);
+    ROS_INFO_STREAM("Iteration: " << iteration << "  "
+                                  << "Time: " << elapsed << "  "
+                                  << "Sampling: " << aep_ac.getResult()->sampling_time.data << "  "
+                                  << "Planning: " << aep_ac.getResult()->planning_time.data << "  "
+                                  << "Collision check: " << aep_ac.getResult()->collision_check_time.data << "  "
+                                  << "Flying: " << fly_time << " "
+                                  << "Tree size: " << aep_ac.getResult()->tree_size);
 
-    logfile << iteration << ", " 
-            << elapsed << ", "
-            << aep_ac.getResult()->sampling_time.data << ", "
-            << aep_ac.getResult()->planning_time.data << ", "
-            << aep_ac.getResult()->collision_check_time.data << ", "
-            << fly_time << ", "
-            << aep_ac.getResult()->tree_size << std::endl;
+    logfile << iteration << ", " << elapsed << ", " << aep_ac.getResult()->sampling_time.data << ", "
+            << aep_ac.getResult()->planning_time.data << ", " << aep_ac.getResult()->collision_check_time.data << ", "
+            << fly_time << ", " << aep_ac.getResult()->tree_size << std::endl;
 
     iteration++;
   }
